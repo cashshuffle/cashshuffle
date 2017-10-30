60c73624226a8788da554a4222e986438f2161dc
Josh Ellithorpe <quest@mac.com>
2017-10-29 18:05:54 -0700

    Added unicast

--- server/broadcast.go
+++ server/broadcast.go
@@ -1,7 +1,34 @@
 package server
 
+import "errors"
+
 // broadcastMessage processes messages and either broadcasts
 // them to all connected users, or to a single user.
 func (sc *signedConn) broadcastMessage() error {
+	to := sc.message.GetPacket().GetTo().String()
+
+	if to == "" {
+		err := sc.broadcastAll()
+		if err != nil {
+			return err
+		}
+	} else {
+		td := sc.tracker.getVerificationKeyData(to)
+		if td == nil {
+			return errors.New("peer disconnected")
+		}
+
+		err := writeMessage(td.conn, sc.message)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// broadcastAll broadcasts to all participants.
+func (sc *signedConn) broadcastAll() error {
+
 	return nil
 }
