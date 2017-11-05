bd116668ab6b0b9ce1eb507045751917d48661af
Josh Ellithorpe <quest@mac.com>
2017-11-05 14:07:54 -0800

    Delay announcement message for 50 milliseconds

--- server/messages.go
+++ server/messages.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"time"
 
 	"github.com/cashshuffle/cashshuffle/message"
 	"github.com/golang/protobuf/proto"
@@ -46,6 +47,7 @@ func (sc *signedConn) processReceivedMessage() error {
 		playerData := sc.tracker.getTrackerData(sc.conn)
 
 		if sc.tracker.getPoolSize(playerData.pool) == sc.tracker.poolSize {
+			time.Sleep(50 * time.Millisecond)
 			sc.announceStart()
 		}
 
