291c3e95d11153ec8c4d6c6454c2930a7ff0980a
emergent-reasons <emergentreasons@gmail.com>
2019-03-11 00:35:47 +0900

    e2e testing/demo of happy path

--- server/e2e_test.go
+++ server/e2e_test.go
@@ -20,32 +20,32 @@ var (
 type testHarness struct {
 	tracker *Tracker
 	packets chan *packetInfo
-	T       *testing.T
+	t       *testing.T
 }
 
 type testClient struct {
-	h          *testHarness
-	conn       net.Conn
-	Inbox      *testInbox
-	amount     uint64
-	version    uint64
-	session    []byte
-	playerNum  uint32
+	h         *testHarness
+	conn      net.Conn
+	inbox     *testInbox
+	amount    uint64
+	version   uint64
+	session   []byte
+	playerNum uint32
 }
 
 // new TestHarness sets up the required parts for automating a shuffle
-func testNewHarness(t *testing.T) *testHarness {
+func testNewHarness(t *testing.T, poolSize int) *testHarness {
 	// prepare shuffle environment: tracker, packet channel, connections
 	anyPort := 0
-	poolSize := 4
-	tracker := NewTracker(poolSize, anyPort , anyPort , anyPort , anyPort)
-
+	tracker := NewTracker(poolSize, anyPort, anyPort, anyPort, anyPort)
 	piChan := make(chan *packetInfo)
 	go startPacketInfoChan(piChan)
+
 	return &testHarness{
 		tracker: tracker,
 		packets: piChan,
-		T:       t}
+		t:       t,
+	}
 }
 
 // NewClient creates a client with an in-memory connection to server
@@ -59,18 +59,18 @@ func (h *testHarness) NewClient() *testClient {
 	inbox := testNewInbox(clientConn)
 
 	// return a client that has done nothing but connect
-	return &testClient {
-		h:          h,
-		conn:       clientConn,
-		Inbox:      inbox,
+	return &testClient{
+		h:     h,
+		conn:  clientConn,
+		inbox: inbox,
 	}
 }
 
 // Register has each connection request a registration
 // https://github.com/cashshuffle/cashshuffle/wiki/CashShuffle-Server-Specification#entering-a-shuffle
-func (tc *testClient) Register(amount, version uint64) {
-	tc.amount = amount
-	tc.version = version
+func (client *testClient) Register(amount, version uint64) {
+	client.amount = amount
+	client.version = version
 
 	testTempKey++
 	registrationMessage := &message.Signed{
@@ -79,23 +79,20 @@ func (tc *testClient) Register(amount, version uint64) {
 				Key: strconv.Itoa(testTempKey),
 			},
 			Registration: &message.Registration{
-				Amount: amount,
+				Amount:  amount,
 				Version: version,
-				Type: message.ShuffleType_DEFAULT,
+				Type:    message.ShuffleType_DEFAULT,
 			},
 		},
 		Signature: nil,
 	}
-	err := writeMessage(tc.conn, []*message.Signed{registrationMessage})
+
+	err := writeMessage(client.conn, []*message.Signed{registrationMessage})
 	if err != nil {
-		tc.h.T.Fatal(err)
+		client.h.t.Fatal(err)
 	}
 
-	// check the response
-	//response := tc.Inbox.WaitOldest()
-	//fmt.Println("Register: got response:", response)
-	//tc.playerNum = response.message.GetPacket()[0].Packet.Number
-	//tc.session = response.message.GetPacket()[0].Packet.Session
+	client.playerNum, client.session = client.h.AssertRegistered(client)
 }
 
 type testInbox struct {
@@ -104,63 +101,179 @@ type testInbox struct {
 }
 
 func testNewInbox(conn net.Conn) *testInbox {
+	ti := &testInbox{
+		packets: make([]*packetInfo, 0),
+		mutex:   sync.Mutex{},
+	}
 	packetChan := make(chan *packetInfo)
-	packets := make([]*packetInfo, 0)
-	m := sync.Mutex{}
+
+	// a client-side tracker is needed just for its channel closing machinery
+	anyValue := 10
+	placeholderTracker := NewTracker(anyValue, anyValue, anyValue, anyValue, anyValue)
+	go processMessages(conn, packetChan, placeholderTracker)
 	go func() {
-		processMessages(conn, packetChan, nil)
 		for pi := range packetChan {
-			m.Lock()
-			packets = append(packets, pi)
-			m.Unlock()
-			fmt.Println("received inbox message")
+			ti.mutex.Lock()
+			ti.packets = append(ti.packets, pi)
+			ti.mutex.Unlock()
 		}
 	}()
-	return &testInbox{
-		packets: packets,
-		mutex:   m,
-	}
+	return ti
 }
 
-func (ib *testInbox) WaitOldest() *packetInfo {
-	fmt.Println("WaitOldest: start waiting")
-	for {
+func (ib *testInbox) PopOldest() (*packetInfo, error) {
+	var p *packetInfo
+
+	for i := 0; i < 2; i++ {
+		ib.mutex.Lock()
 		if len(ib.packets) > 0 {
-			break
+			p = ib.packets[0]
+			ib.packets = ib.packets[1:]
+			ib.mutex.Unlock()
+			return p, nil
 		}
-		time.Sleep(10 * time.Millisecond)
+		ib.mutex.Unlock()
+		time.Sleep(1 * time.Millisecond)
 	}
-
-	ib.mutex.Lock()
-	packet := ib.packets[0]
-	ib.packets = ib.packets[1:]
-	ib.mutex.Unlock()
-	return packet
+	return nil, fmt.Errorf("empty inbox")
 }
 
+type poolState struct {
+	value   uint64
+	version uint64
+	players int
+	isFull  bool
+}
 
-func TestRegistration(t *testing.T) {
-	h := testNewHarness(t)
-	clients := []*testClient{
-		h.NewClient(),
-		//h.NewClient(),
-		//h.NewClient(),
-	}
-
+func TestHappyShuffle(t *testing.T) {
+	poolSize := 3
+	h := testNewHarness(t, poolSize)
 	bch1 := uint64(100000000)
 	version := uint64(999)
-	for _, client := range clients {
+
+	// clients join the pool, one at a time
+	clients := make([]*testClient, 0)
+	for i := 0; i < poolSize; i++ {
+		client := h.NewClient()
+		clients = append(clients, client)
+
 		client.Register(bch1, version)
+		if i < poolSize-1 {
+			h.AssertBroadcastNewPlayer(client, clients)
+		}
+	}
+	// the pool is full and the shuffle starts
+	h.AssertBroadcastPhase1Announcement(clients)
+
+	// confirm basic state on the server side
+	h.AssertPools([]poolState{
+		{
+			value:   bch1,
+			version: version,
+			players: len(clients),
+			isFull:  true,
+		},
+	})
+
+	// the shuffle succeeded, and clients leave with no blame
+	for _, c := range clients {
+		err := c.conn.Close()
+		if err != nil {
+			t.Fatal(err)
+		}
 	}
 
+	// after the clients leave, the pool should be removed
+	h.AssertPools([]poolState{})
 
-	fmt.Println("inbox before: ", clients[0].Inbox.packets)
-	time.Sleep(time.Second * 1)
-	fmt.Println("inbox after: ", clients[0].Inbox.packets)
+	// all messages should be consumed through the assertions
+	// if anything is remaining, it is outside of specification
+	// or something unexpected happened
+	h.AssertEmptyInboxes(clients)
+}
 
-	assert.Equal(t, 3, len(h.tracker.connections))
-	//assert.Equal(t, 1, len(h.Tracker.pools))
-	//assert.Equal(t, map[int]uint64{1: bch1}, h.Tracker.poolAmounts)
-	//assert.Equal(t, map[int]message.ShuffleType{1: message.ShuffleType_DEFAULT}, h.Tracker.poolTypes)
-	//assert.Equal(t, map[int]uint64{1: 999}, h.Tracker.poolVersions)
+func (h *testHarness) AssertRegistered(c *testClient) (uint32, []byte) {
+	response, err := c.inbox.PopOldest()
+	if err != nil {
+		h.t.Fatal(err)
+	}
+	signedPackets := response.message.GetPacket()
+	assert.Len(h.t, signedPackets, 1)
+	packet := signedPackets[0].Packet
+
+	playerNum := packet.GetNumber()
+	assert.NotEqual(h.t, 0, playerNum)
+
+	session := packet.GetSession()
+	assert.NotEqual(h.t, 0, len(session))
+
+	return playerNum, session
+}
+
+func (h *testHarness) AssertBroadcastNewPlayer(c *testClient, all []*testClient) {
+	for _, eachClient := range all {
+		response, err := eachClient.inbox.PopOldest()
+		if err != nil {
+			h.t.Fatal(err)
+		}
+		signedPackets := response.message.GetPacket()
+		assert.Len(h.t, signedPackets, 1)
+		packet := signedPackets[0].Packet
+
+		assert.Equal(h.t, c.playerNum, packet.GetNumber())
+	}
+}
+
+func (h *testHarness) AssertBroadcastPhase1Announcement(all []*testClient) {
+	for _, eachClient := range all {
+		response, err := eachClient.inbox.PopOldest()
+		if err != nil {
+			h.t.Fatal(err)
+		}
+		signedPackets := response.message.GetPacket()
+		assert.Len(h.t, signedPackets, 1)
+		packet := signedPackets[0].Packet
+
+		assert.Equal(h.t, message.Phase_ANNOUNCEMENT, packet.GetPhase())
+		assert.Equal(h.t, uint32(h.tracker.poolSize), packet.GetNumber())
+	}
+}
+
+func (h *testHarness) AssertPools(states []poolState) {
+	// wait for the server to catch up
+	for i := 0; i < 2; i++ {
+		if len(h.tracker.pools) != len(states) {
+			time.Sleep(1 * time.Millisecond)
+			continue
+		}
+		break
+	}
+
+	// convert pools into simple states
+	actualStates := make([]poolState, 0)
+	for poolNum := range h.tracker.pools {
+		_, isFull := h.tracker.fullPools[poolNum]
+		actualStates = append(actualStates, poolState{
+			value:   h.tracker.poolAmounts[poolNum],
+			version: h.tracker.poolVersions[poolNum],
+			players: len(h.tracker.pools[poolNum]),
+			isFull:  isFull,
+		})
+	}
+
+	// check for each state in the pools
+	assert.ElementsMatch(h.t, states, actualStates)
+}
+
+func (h *testHarness) AssertEmptyInboxes(clients []*testClient) {
+	for _, c := range clients {
+		c.inbox.mutex.Lock()
+		if len(c.inbox.packets) != 0 {
+			e := fmt.Sprintf("inbox #%d not empty:\n", c.playerNum)
+			for _, pkt := range c.inbox.packets {
+				e = e + fmt.Sprintf("  %+v\n", pkt.message.GetPacket())
+			}
+			h.t.Fatalf(e)
+		}
+	}
 }
