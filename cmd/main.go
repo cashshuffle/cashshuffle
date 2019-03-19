01c99e94227df1009bd5d689e231bd2b5f760d44
Josh Ellithorpe <quest@mac.com>
2019-03-19 00:25:51 -0700

    Bump version

--- cmd/main.go
+++ cmd/main.go
@@ -18,7 +18,7 @@ import (
 
 const (
 	appName                 = "cashshuffle"
-	version                 = "0.6.12"
+	version                 = "0.6.13"
 	defaultPort             = 1337
 	defaultWebSocketPort    = 1338
 	defaultTorPort          = 1339
