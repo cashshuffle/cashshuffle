97a34a62ca735227edbb01b4dd023df0817e3f40
quest <quest@mac.com>
2019-03-28 09:12:07 -0700

    Use consistent casing for printed messages

--- server/tracker.go
+++ server/tracker.go
@@ -177,7 +177,7 @@ func (t *Tracker) CleanupDeniedByIPMatch() {
 		if deniedTime.Add(denyIPTime).Before(time.Now()) {
 			delete(t.denyIPMatch, pair)
 			if debugMode {
-				fmt.Printf("[CleanupDenyIPMatch] remove match %s %s\n", pair.left, pair.right)
+				fmt.Printf("[CleanupDenyIPMatch] Remove match %s %s\n", pair.left, pair.right)
 			}
 		}
 	}
@@ -218,7 +218,7 @@ func (t *Tracker) cleanupBan(ip string) {
 	if t.banData[ip].score == 0 {
 		delete(t.banData, ip)
 		if debugMode {
-			fmt.Printf("[CleanupDenyIP] remove server ban %s\n", ip)
+			fmt.Printf("[CleanupDenyIP] Remove server ban %s\n", ip)
 		}
 	}
 }
