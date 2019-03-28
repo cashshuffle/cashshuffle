653910e20515e216ca9450e3ee78d12bdf12d014
quest <quest@mac.com>
2019-03-28 09:12:37 -0700

    Bump version to 0.6.17

--- cmd/main.go
+++ cmd/main.go
@@ -18,7 +18,7 @@ import (
 
 const (
 	appName                 = "cashshuffle"
-	version                 = "0.6.16"
+	version                 = "0.6.17"
 	defaultPort             = 1337
 	defaultWebSocketPort    = 1338
 	defaultTorPort          = 1339
